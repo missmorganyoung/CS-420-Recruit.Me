ea7347bda78034b6b3e197bd68aa9230f16c620b
Morgan Young <52981150+missmorganyoung@users.noreply.github.com>
2023-05-14 20:42:36 -0700

    Create LikesView.swift

---
+++ BuildRecruitMe/MatchesView/LikesView.swift
@@ -0,0 +1,62 @@
+//  RecruitMe mobile app
+//  Created by Morgan Young
+//  LikesView
+
+import SwiftUI
+
+struct LikesView: View {
+    @EnvironmentObject var userMng: UserManager
+    @EnvironmentObject var appState: AppStateManager
+    
+    private var user: User {
+        return userMng.currentUser
+    }
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false, content: {
+            
+            if !user.goldSubscriber {
+                Text("Upgrade to Premium to access Resouces Marketplace.")
+                    .multilineTextAlignment(.center)
+                    .lineLimit(2)
+                    .foregroundColor(.textTitle)
+                    .font(.system(size: 16, weight: .medium))
+                    .padding(.horizontal, 40)
+                    .padding(.vertical, 24)
+            }
+            
+            LazyVGrid(
+                columns: [GridItem(.flexible()), GridItem(.flexible())],
+                alignment: .center,
+                spacing: nil,
+                pinnedViews: [],
+                content: {
+                    ForEach(userMng.matches) { person in
+                        PersonSquare(person: person, blur: !user.goldSubscriber)
+                            .frame(height: 240)
+                            .onTapGesture(perform: {
+                                personTapped(person)
+                            })
+                    }
+                })
+                .padding(.horizontal, 6)
+            
+        })
+    }
+    
+    func personTapped(_ person: Person) {
+        if user.goldSubscriber {
+            appState.showPersonsProfile(person)
+        } else {
+            appState.showPurchaseScreen()
+        }
+    }
+}
+
+struct LikesView_Previews: PreviewProvider {
+    static var previews: some View {
+        LikesView()
+            .environmentObject(UserManager())
+            .environmentObject(AppStateManager())
+    }
+}

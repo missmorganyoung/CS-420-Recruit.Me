8274b2b33d177496dfc4e549b5a80d8e022c07ea
Morgan Young <52981150+missmorganyoung@users.noreply.github.com>
2023-05-14 21:07:07 -0700

    Create RoundedCorner.swift

---
+++ BuildRecruitMe/Extensions/RoundedCorner.swift
@@ -0,0 +1,22 @@
+//  RecruitMe mobile app
+//  Created by Morgan Young
+//  RoundedCorner
+
+import SwiftUI
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}

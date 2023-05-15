88d4e53a77fb04f043b0e90303a7e3aa5f6714bb
Morgan Young <52981150+missmorganyoung@users.noreply.github.com>
2023-05-14 20:33:10 -0700

    Update ProfileView.swift

--- BuildRecruitMe/Profile/ProfileView.swift
+++ BuildRecruitMe/Profile/ProfileView.swift
@@ -4,68 +4,155 @@
 
 import SwiftUI
 
-struct HomeView: View {
-    
+struct ProfileView: View {
     @EnvironmentObject var userMng: UserManager
     @EnvironmentObject var appState: AppStateManager
     
+    var user: User {
+        return userMng.currentUser
+    }
+    
     var body: some View {
-        VStack {
-            CardStack(people: userMng.cardPeople)
-            
-            Spacer()
-            
-            HStack {
-                CircleButtonView(type: .back) {
-                    appState.showPurchaseScreen()
-                }
+        VStack(spacing: 0) {
+            ZStack(alignment: .topTrailing) {
+                RoundedImage(url: user.imageURLS.first)
+                    .frame(height: 175)
                 
-                Spacer()
-                
-                CircleButtonView(type: .no) {
-                    if let person = userMng.cardPeople.last {
-                        userMng.swipe(person, .nope)
-                    }
-                }
-                
-                Spacer()
-                
-                CircleButtonView(type: .star) {
-                    if let person = userMng.cardPeople.last {
-                        if userMng.currentUser.goldSubscriber {
-                            userMng.superLike(person)
-                        } else {
-                            appState.showPurchaseScreen()
-                        }
-                    }
-                }
-                
-                Spacer()
-                
-                CircleButtonView(type: .heart) {
-                    if let person = userMng.cardPeople.last {
-                        userMng.swipe(person, .like)
-                    }
-                }
-                
-                Spacer()
-                
-                CircleButtonView(type: .lightning) {
-                    appState.showPurchaseScreen()
-                }
+                Button(action: {  }, label: {
+                    Image(systemName: "pencil")
+                        .font(.system(size: 18, weight: .heavy))
+                        .foregroundColor(Color.gray.opacity(0.5))
+                        .frame(width: 32, height: 32)
+                        .background(Color.white)
+                        .clipShape(Circle())
+                        .shadow(radius: 6)
+                })
+                .padding(.vertical, 10)
+                .offset(x: -10)
             }
-            .frame(height: 50)
-            .padding(.horizontal)
-            .padding(.vertical, 25)
+            
+            Spacer().frame(height: 18)
+            
+            // Name + Job Title
+            Group {
+                Text("\(user.name)")
+                    .foregroundColor(.textTitle)
+                    .font(.system(size: 26, weight: .medium))
+                
+                Spacer().frame(height: 3)
+                
+                Text("\(user.jobTitle) @ \(user.company)")
+                Text("Attending \(user.school)")
+                
+                Spacer().frame(height: 22)
+            }
+            
+            HStack(alignment: .top) {
+                Spacer()
+                
+                Button(action: {  }, label: {
+                    VStack {
+                        Image(systemName: "gearshape.fill")
+                            .foregroundColor(Color.gray.opacity(0.5))
+                            .font(.system(size: 30))
+                            .padding(10)
+                            .background(Color.white)
+                            .clipShape(Circle())
+                            .shadow(radius: 6)
+                        
+                        Text("SETTINGS")
+                            .font(.system(size: 14, weight: .medium))
+                            .foregroundColor(.textSecondary)
+                    }
+                })
+                
+                Spacer()
+                
+                Button(action: {  }, label: {
+                    VStack {
+                        Image(systemName: "camera.fill")
+                            .foregroundColor(Color.white)
+                            .font(.system(size: 38))
+                            .padding(22)
+                            .background(Color.myTeal)
+                            .clipShape(Circle())
+                            .shadow(radius: 6)
+                        
+                        Text("ADD MEDIA")
+                            .font(.system(size: 14, weight: .medium))
+                            .foregroundColor(.textSecondary)
+                    }
+                })
+                
+                Spacer()
+                
+                
+                Button(action: {  }, label: {
+                    VStack {
+                        Image(systemName: "shield.fill")
+                            .foregroundColor(Color.gray.opacity(0.5))
+                            .font(.system(size: 30))
+                            .padding(10)
+                            .background(Color.white)
+                            .clipShape(Circle())
+                            .shadow(radius: 6)
+                        
+                        Text("SAFETY")
+                            .font(.system(size: 14, weight: .medium))
+                            .foregroundColor(.textSecondary)
+                    }
+                })
+                
+                Spacer()
+            }
+            
+            Spacer().frame(height: 14)
+            
+            if !user.profileTip.isEmpty {
+                HStack {
+                    Text("\(user.profileTip)")
+                        .multilineTextAlignment(.leading)
+                        .lineLimit(3)
+                        .foregroundColor(.white)
+                        .font(.system(size: 14))
+                    
+                    Button(action: {  }, label: {
+                        Image(systemName: "plus")
+                            .font(.system(size: 12, weight: .heavy))
+                            .foregroundColor(.myTeal)
+                            .padding(6)
+                        
+                    })
+                    .background(Color.white)
+                    .clipShape(Circle())
+                }
+                .padding()
+                .background(Color.myTeal)
+                .cornerRadius(12)
+                .padding(.horizontal, 8)
+            }
+            
+            if !user.goldSubscriber {
+                ZStack {
+                    Color.gray.opacity(0.15)
+                    
+                    ProfileSwipePromo {
+                        appState.showPurchaseScreen()
+                    }
+                }
+                .padding(.top, 18)
+            }
             
             Spacer()
         }
+        .foregroundColor(Color.black.opacity(0.75))
     }
 }
 
-struct HomeView_Previews: PreviewProvider {
+struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        ProfileView()
+            .background(Color.defaultBackground)
             .environmentObject(UserManager())
             .environmentObject(AppStateManager())
     }

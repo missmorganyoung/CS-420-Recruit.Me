840e425537b1fd229f43ae53e07616dcef8ad8d9
Morgan Young <52981150+missmorganyoung@users.noreply.github.com>
2023-05-14 19:51:47 -0700

    Create BuildRecruitMe.swift file

---
+++ BuildRecruitMe.swift
@@ -0,0 +1,14 @@
+//  RecruitMe mobile app
+// BuildRecruitMe
+//  Created by Morgan Young
+
+import SwiftUI
+
+@main
+struct BuildTinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
